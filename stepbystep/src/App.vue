c1ef51bfb61e76f7ca68a8a0e231f4f8027a9550
jongmin1 <jm00512@naver.com>
2024-10-04 10:49:26 +0900

    add: stepbystep

---
+++ stepbystep/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page">TEST</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.page {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100vh;
+
+  background-color: black;
+  color: white;
+}
+</style>
