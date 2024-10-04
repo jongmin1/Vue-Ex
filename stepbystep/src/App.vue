2a4a9f0ae287871478750e852cd89d964edf1bed
jongmin1 <jm00512@naver.com>
2024-10-04 11:19:46 +0900

    feat: step2

--- stepbystep/src/App.vue
+++ stepbystep/src/App.vue
@@ -1,21 +1,7 @@
 <template>
-  <div class="page">TEST</div>
+  <RouterView />
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style lang="scss" scoped>
-.page {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  width: 100%;
-  height: 100vh;
-
-  background-color: black;
-  color: white;
-}
-</style>
+<style lang="scss" scoped></style>
