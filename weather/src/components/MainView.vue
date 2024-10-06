2a66fd27f3375468122b50a7e5745ebb72e134e4
jongmin1 <jm00512@naver.com>
2024-10-06 14:15:21 +0900

    add: weather 개발환경 구축

---
+++ weather/src/components/MainView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="leftContainer">
+        <div id="cityNameBox">
+            <div class="cityName">
+                <p>San Fransisco</p>
+                <p>Jan 28, 2022</p>
+            </div>
+        <div class="contentsBox">
+            <div class="buttonBox">
+                <div class="buttonBackground">
+                    <button class="forecase">Forecast</button>
+                    <button class="airquality">Air Quality</button>
+                </div>
+            </div>
+        <div class="weatherBox">
+            <div class="weatherDgree">
+                <p>10&deg;</p>
+            </div> 
+            div
+        </div>
+        </div>
+        </div>
+    </div>
+</template>
---
+++ weather/src/pages/home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="wrap">
+        <div class="container"></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.warp {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 100vh;
+    font-size: 16px;
+
+    .container {
+        display: center;
+        align-items: center;
+        justify-content: space-between;
+
+        width: 55%;
+        height: 90%;
+        max-width: 1920px;
+    }
+}
+</style>
