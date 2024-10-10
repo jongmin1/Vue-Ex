d8fee127fb3ae07948da476f24ff939341617669
jongmin1 <jm00512@naver.com>
2024-10-10 14:39:32 +0900

    feat: completed

--- tutrorial/bootstrap/src/components/organisms/SideBar.vue
+++ tutrorial/bootstrap/src/components/organisms/SideBar.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import LineChart from '../atoms/Charts/LineChart.vue';
+
+onMounted(() => {
+  const sidebarToggle = document.querySelector("#sidebar-toggle");
+  sidebarToggle?.addEventListener("click", function() {
+    const sidebar = document.querySelector('#sidebar');
+    sidebar?.classList.toggle("collapsed");
+  });
+});
+</script>
+
 <template>
   <div class="wrapper">
     <aside id="sidebar">
@@ -22,14 +35,30 @@
           </a>
           <ul id="pages" class="sidebar-dropdown list-unstyled collase" data-bs-parent="#sidebar">
             <li class="sidebar-item">
-              <a href="#" class="sidebar-link">Page 1</a>
+              <a href="#" class="sidebar-link mx-3">Page 1</a>
             </li>
             <li class="sidebar-item">
-              <a href="#" class="sidebar-link">Page 2</a>
+              <a href="#" class="sidebar-link mx-3">Page 2</a>
             </li>
           </ul>
           </li>
-          
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link collapsed" data-bs-target="#posts"
+            data-bs-toggle="collapse" aria-expanded="false"><i class="fa-solid fa-file-lines pe-2"></i>
+            Posts
+          </a>
+          <ul id="posts" class="sidebar-dropdown list-unstyled collase" data-bs-parent="#sidebar">
+            <li class="sidebar-item">
+              <a href="#" class="sidebar-link mx-3">Post 1</a>
+            </li>
+            <li class="sidebar-item">
+              <a href="#" class="sidebar-link mx-3">Post 2</a>
+            </li>
+            <li class="sidebar-item">
+              <a href="#" class="sidebar-link mx-3">Post 3</a>
+            </li>
+          </ul>
+          </li>
           <li class="sidebar-item">
             <a href="#" class="sidebar-link collapsed" data-bs-target="#auth"
             data-bs-toggle="collapse" aria-expanded="false"><i class="fa-regualr fa-user pe-2"></i>
@@ -37,212 +66,197 @@
           </a>
           <ul id="auth" class="sidebar-dropdown list-unstyled collase" data-bs-parent="#sidebar">
             <li class="sidebar-item">
-              <a href="#" class="sidebar-link">Login</a>
+              <a href="#" class="sidebar-link mx-3">Login</a>
             </li>
             <li class="sidebar-item">
-              <a href="#" class="sidebar-link">Register</a>
+              <a href="#" class="sidebar-link mx-3">Register</a>
             </li>
             <li class="sidebar-item">
-              <a href="#" class="sidebar-link">Forgor Password</a>
+              <a href="#" class="sidebar-link mx-3">Forgor Password</a>
             </li>
+          </ul>
+          </li>
+          <li class="sidebar-header">
+            Multi Level Menu
+          </li>
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link collapsed" data-bs-target="#multi"
+              data-bs-toggle="collapse" aria-expanded="false"><i class="fa-regualr fa-user pe-2"></i>
+              Multi Dropdown
+          </a>
+          <ul id="multi" class="sidebar-dropdown list-unstyled collase" data-bs-parent="#sidebar">
+            <li class="sidebar-item">
+              <a href="#" class="sidebar-link collapsed mx-3" data-bs-target="#level-1"
+              data-bs-toggle="collapse" aria-expanded="false">Level 1</a>
+              <ul id="level-1" class="sidebar-dropdown list-unstyled collase">
+                <li class="sidebar-item">
+                  <a href="#" class="sidebar-link mx-4">Level 1-1</a>
+                </li>
+                <li class="sidebar-item">
+                  <a href="#" class="sidebar-link mx-4">Level 1-2</a>
+                </li>
+            </ul>
+            </li>
+            
           </ul>
           </li>
         </ul>
       </div>
     </aside>
     <div class="main">
-      <nav class="navbar">
-        <button class="btn">
+      <nav class="navbar navbar-expand px-3 border-bottom">
+        <button class="btn" id="sidebar-toggle" type="button">
           <span class="navbar-toggler-icon"></span>
         </button>
+        <div class="navbar-collapse navbar">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a href="#" data-bs-toggle="dropdown" class="nav-icon pe-md-0">
+                <img src="../../assets/images/profile.jpg" class="avatar img-fluid rounded" alt="">
+              </a>
+              <div class="dropdown-menu dropdown-menu-end">
+                <a href="#" class="dropdown-item ">Profile</a>
+                <a href="#" class="dropdown-item">Setting</a>
+                <a href="#" class="dropdown-item">Logout</a>
+              </div>
+            </li>
+          </ul>
+        </div>
       </nav>
+      <main class="content px-3 py-2">
+        <div class="container-fluid">
+          <div class="mb-3">
+            <h4>Admin Dashboard</h4>
+          </div>
+          <div class="row">
+            <div class="col-12 col-md-6 d-flex">
+              <div class="card flex-fill border-0 illustration">
+                <div class="card-body p-0 d-flex flex-fill">
+                  <div class="row g-0 w-100">
+                    <div class="col-6">
+                      <div class="p-3 m-1">
+                        <h4>Welcome Back, Admin</h4>
+                        <p class="mb-0">Admin Dashboard</p>
+                      </div>
+                    </div>
+                    <div class="col-6 align-self-end text-end">
+                      <img src="../../assets/images/customer-support.jpg" class="img-fluid illustration-img" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-6 d-flex">
+              <div class="card  flex-fill border-0">
+                <div class="card-body py-4">
+                  <div class="d-flex align-items-start">
+                    <div class="flex-grow-1">
+                      <h4 class="mb-2">
+                        $ 78.0
+                      </h4>
+                      <p class="mb-2">
+                        Total Earnings
+                      </p>
+                      <div class="mb-0">
+                        <span class="badge text-success me-2">
+                          +9.0%
+                        </span>
+                        <span class="text-muted">
+                          Since Last Month
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Table Element -->
+           <div class="card border-0">
+            <div class="card-header">
+              <h5 class="card-title">
+                Basic Table
+              </h5>
+              <h6 class="card-subtitle  text-muted">
+                Lprem ipsum dolor sit am.,asmdfmsaldfkjlasdjflksdjf
+              </h6>
+            </div>
+            <div class="card-body">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+           </div>
+           <div class="card border-0">
+            <div class="card-header">
+              <h5 class="card-title">
+                Line Chart
+              </h5>
+            </div>
+            <div class="card-body">
+              <LineChart />
+            </div>
+            </div>
+        </div>
+      </main>
+      <footer class="footer">
+        <div class="container-fluid">
+          <div class="row text-muted">
+            <div class="col-6 text-start">
+              <p class="mb-0">
+                <a href="#" class="text-muted">
+                  <strong>Jongmin</strong>
+                </a>
+              </p>
+            </div>
+            <div class="col-6 text-end">
+              <ul class="list-inline">
+                <li class="list-inline-item">
+                  <a href="#" class="text-muted">Contact</a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#" class="text-muted">About Us</a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#" class="text-muted">Terms</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   </div>
 </template>
 
-<style>
-a {
-    cursor: pointer;
-    text-decoration: none;
-    font-family: 'Poppins', sans-serif;
-}
 
-li {
-    list-style: none;
-}
-
-h4 {
-    font-family: 'Poppins', sans-serif;
-    font-size: 1.275rem;
-    color: var(--bs-emphasis-color);
-}
-
-/* Layout for admin dashboard skeleton */
-
-.wrapper {
-    align-items: stretch;
-    display: flex;
-    width: 100%;
-}
-
-#sidebar {
-    max-width: 264px;
-    min-width: 264px;
-    background: var(--bs-dark);
-    transition: all 0.35s ease-in-out;
-}
-
-.main {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    min-width: 0;
-    overflow: hidden;
-    transition: all 0.35s ease-in-out;
-    width: 100%;
-    background: var(--bs-dark-bg-subtle);
-}
-
-/* Sidebar Elements Style */
-
-.sidebar-logo {
-    padding: 1.15rem;
-}
-
-.sidebar-logo a {
-    color: #e9ecef;
-    font-size: 1.15rem;
-    font-weight: 600;
-}
-
-.sidebar-nav {
-    list-style: none;
-    margin-bottom: 0;
-    padding-left: 0;
-    margin-left: 0;
-}
-
-.sidebar-header {
-    color: #e9ecef;
-    font-size: .75rem;
-    padding: 1.5rem 1.5rem .375rem;
-}
-
-a.sidebar-link {
-    padding: .625rem 1.625rem;
-    color: #e9ecef;
-    position: relative;
-    display: block;
-    font-size: 0.875rem;
-}
-
-.sidebar-link[data-bs-toggle="collapse"]::after {
-    border: solid;
-    border-width: 0 .075rem .075rem 0;
-    content: "";
-    display: inline-block;
-    padding: 2px;
-    position: absolute;
-    right: 1.5rem;
-    top: 1.4rem;
-    transform: rotate(-135deg);
-    transition: all .2s ease-out;
-}
-
-.sidebar-link[data-bs-toggle="collapse"].collapsed::after {
-    transform: rotate(45deg);
-    transition: all .2s ease-out;
-}
-
-.avatar {
-    height: 40px;
-    width: 40px;
-}
-
-.navbar-expand .navbar-nav {
-    margin-left: auto;
-}
-
-.content {
-    flex: 1;
-    max-width: 100vw;
-    width: 100vw;
-}
-
-@media (min-width:768px) {
-    .content {
-        max-width: auto;
-        width: auto;
-    }
-}
-
-.card {
-    box-shadow: 0 0 .875rem 0 rgba(34, 46, 60, .05);
-    margin-bottom: 24px;
-}
-
-.illustration {
-    background-color: var(--bs-primary-bg-subtle);
-    color: var(--bs-emphasis-color);
-}
-
-.illustration-img {
-    max-width: 150px;
-    width: 100%;
-}
-
-/* Sidebar Toggle */
-
-#sidebar.collapsed {
-    margin-left: -264px;
-}
-
-/* Footer and Nav */
-
-@media (max-width:767.98px) {
-
-    .js-sidebar {
-        margin-left: -264px;
-    }
-
-    #sidebar.collapsed {
-        margin-left: 0;
-    }
-
-    .navbar,
-    footer {
-        width: 100vw;
-    }
-}
-
-/* Theme Toggler */
-
-.theme-toggle {
-    position: fixed;
-    top: 50%;
-    transform: translateY(-65%);
-    text-align: center;
-    z-index: 10;
-    right: 0;
-    left: auto;
-    border: none;
-    background-color: var(--bs-body-color);
-}
-
-html[data-bs-theme="dark"] .theme-toggle .fa-sun,
-html[data-bs-theme="light"] .theme-toggle .fa-moon {
-    cursor: pointer;
-    padding: 10px;
-    display: block;
-    font-size: 1.25rem;
-    color: #FFF;
-}
-
-html[data-bs-theme="dark"] .theme-toggle .fa-moon {
-    display: none;
-}
-
-html[data-bs-theme="light"] .theme-toggle .fa-sun {
-    display: none;
-}
+<style lang="scss" scoped>
+@import '../../assets/styles/main.scss'
 </style>
